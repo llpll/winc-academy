7fdd58a5e49e7341f5ba107355f6573b11f915ca
llpll <45404963+llpll@users.noreply.github.com>
2021-03-18 12:23:47 +0100

    correction requirements

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Hallo Winc Academy studenten");
 
 // Dit is een grote som
-/* let multiply = 1230941 * 1823794;
+let multiply = 1230941 * 1823794;
 console.log(multiply);
 
-let fraction =  637263 / 54;
+/* let fraction =  637263 / 54;
 console.log(fraction); */
