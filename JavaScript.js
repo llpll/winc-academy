dd7e4a5bf5c93f8aa5a0618171bdd5681a21e562
llpll <45404963+llpll@users.noreply.github.com>
2021-03-18 11:11:35 +0100

    upload

---
+++ JavaScript.js
@@ -0,0 +1,32 @@
+console.log("Hello Winc Academy")
+
+let name = 'Elena';
+console.log('Elena');
+
+let number = 4 + 4;
+console.log(number);
+
+let string = '4 + 4';
+console.log(string);
+
+let substraction = 90 - 10;
+console.log(substraction);
+
+let multiplication = 3 * 20;
+console.log(multiplication);
+
+
+let age = 30;
+console.log(age)
+
+let leeftijd = typeof 30;
+console.log(leeftijd);
+
+let leeftijdString = typeof '30';
+console.log(leeftijdString);
+
+
+
+
+
+
