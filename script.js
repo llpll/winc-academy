e03d7a1e3b45fa784acb057dc6fc37ee483b9314
llpll <45404963+llpll@users.noreply.github.com>
2021-03-19 15:03:49 +0100

    done

---
+++ script.js
@@ -0,0 +1,33 @@
+const age = 18;
+
+if (age >= 18){
+    console.log ('You are legible to enter the bar')
+
+} else if (age < 10) {
+    console.log ('You are just 10!')
+
+} else {
+    console.log ('You are not old enough to have drinks in this bar')
+}
+
+
+const isFemale = true;
+
+if (isFemale){
+    console.log ('You may buy tickets for the ladies night!')
+
+} else {
+    console.log ('Only ladies allowed!')
+}
+
+
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob'){
+    console.log ('You may drive home!')
+
+} else {
+    console.log ('Somebody else has to drive you home!')
+}
+
+
