4a77cc0d028ec5a49eb3749a87cfda9c3216817e
llpll <45404963+llpll@users.noreply.github.com>
2021-03-23 20:59:54 +0100

    upload

--- script.js
+++ script.js
@@ -1,32 +1,104 @@
-console.log("Hello Winc Academy")
+// deel 1 > objects
+let person = {
+    name: 'Elena',
+    age: 35
+}
 
-let name = 'Elena';
-console.log('Elena');
+console.log(person)
 
-let number = 4 + 4;
-console.log(number);
+person.name = "Elena";
+console.log(person.name);
 
-let string = '4 + 4';
-console.log(string);
+person['name'] = 'Elena';
+console.log(person.name);
 
-let substraction = 90 - 10;
-console.log(substraction);
+person.age = 35;
+console.log(person.age);
 
-let multiplication = 3 * 20;
-console.log(multiplication);
-
-
-let age = 30;
-console.log(age)
-
-let leeftijd = 30;
-console.log(typeof leeftijd);
-
-let leeftijdString = '30';
-console.log(typeof leeftijdString);
+person['age'] = 35;
+console.log(person.age);
 
 
+let evaluations = {
+    exam1: 7,
+    exam2: 10,
+    exam3: 9
+}
+console.log(evaluations);
+
+evaluations = 7, 10, 9;
+console.log(evaluations);
+
+// deel 2 > arrays
+let colorPicker = ['green', 'blue', 'red'];
+console.log(colorPicker.length);
+console.log(colorPicker[0]);
+console.log(colorPicker[2]);
+
+let allColors = colorPicker.push('yellow');
+console.log(colorPicker);
+console.log(allColors);
+
+let plusNumber = colorPicker.push(1);
+console.log(colorPicker);
+
+let plusObject = colorPicker.push({ greeting: 'hi ik ben een object'})
+console.log(colorPicker);
+console.log(colorPicker['hi ik ben een object']);
 
 
 
+// deel 3 > 'real-life' object
+const catBreeds = [{
+    name: "Abyssinian",
+    description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+    dog_friendly: 4,
+    energy_level: 5,
+    life_span: "14 - 15",
+    origin: "Egypt",
+    temperament: ["Active", "Energetic", "Independent", "Intelligent", "Gentle"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
+    food: {
+        favourite_food: "fish",
+        medium_liked_food: "dried fruits",
+        disliked_food: "walnuts"
+    }
+},
+{
+    name: "Aegean",
+    description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
+    dog_friendly: 4,
+    energy_level: 53,
+    life_span: "9- 12",
+    origin: "Greece",
+    temperament: ["Affectionate", "Social", "Intelligent", "Playful", "Active"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Aegean_cat",
+    food: {
+        favourite_food: "tuna",
+        medium_liked_food: "canned food",
+        disliked_food: "all fruits"
+    }
+},
+{
+    name: "American Bobtail",
+    description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
+    dog_friendly: 5,
+    energy_level: 3,
+    life_span: "11 - 15",
+    origin: "United States",
+    temperament: ["Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/American_Bobtail",
+    food: {
+        favourite_food: "meaty things",
+        medium_liked_food: "tuna",
+        disliked_food: "canned food"
+    }
+}
+]
 
+console.log(catBreeds[1]);
+console.log(catBreeds[2].name);
+console.log(catBreeds[0].energy_level);
+console.log(catBreeds[1].temperament[0]);
+console.log(catBreeds[2].temperament[4]);
+console.log(catBreeds[2].food.favourite_food);
