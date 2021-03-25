1dcac5a30f0e52057d8fca87b3b0e265b9bf4c7b
llpll <45404963+llpll@users.noreply.github.com>
2021-03-25 10:48:33 +0100

    done

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-console.log("Hello Winc Academy")
 
-let name = 'Elena';
-console.log('Elena');
-
-let number = 4 + 4;
-console.log(number);
-
-let string = '4 + 4';
-console.log(string);
-
-let substraction = 90 - 10;
-console.log(substraction);
-
-let multiplication = 3 * 20;
-console.log(multiplication);
+// declaration
+function calc(no1, no2) {
+    const square1 = no1 * no1;
+    const square2 = no2 * no2;
+    return (square1 + square2) * (square1 + square2);
+}
+console.log(calc(3, 4));
 
 
-let age = 30;
-console.log(age)
-
-let leeftijd = 30;
-console.log(typeof leeftijd);
-
-let leeftijdString = '30';
-console.log(typeof leeftijdString);
-
-
+// expression
+const calculation = function (number1, number2) {
+    const multiplication1 = number1 * number1;
+    const multiplication2 = number2 * number2;
+    const sum = multiplication1 + multiplication2;
+    return sum * sum;
+};
+console.log(calculation(3, 4));
 
 
-
-
+// arrow
+const calculation2 = (num1, num2) => {
+    const num1Multiplicated = num1 * num1;
+    const num2Multiplicated = num2 * num2;
+    return (num1Multiplicated + num2Multiplicated) * (num1Multiplicated + num2Multiplicated);
+};
+console.log(calculation2(3, 4));
