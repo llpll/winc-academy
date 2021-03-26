a211ab813505acac64523d42fbb60fd507737cbb
llpll <45404963+llpll@users.noreply.github.com>
2021-03-26 16:12:02 +0100

    changes

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 
-const btn = document.getElementById('mybutton')
+// part 1
+const btn = document.getElementById('mybutton');
 
 const btnAlert = () => {
     alert('You clicked me!');
 };
 btn.addEventListener('click', btnAlert);
 
-const bckrBtn = document.querySelector('.blue-background:nth-child(2)')
+/* const alertButton = document.getElementById("mybutton");
+alertButton.addEventListener("click", function () {
+  alert("button clicked!");
+}); */
 
-// const changeColor = () => {
-// bckrBtn.classList.add ('red-background') ;
-// };
-// bckrBtn.addEventListener ('click', changeColor);
 
+// part 2
+/* const bckrBtn = document.querySelector('.blue-background:nth-child(2)')
+
+const changeColor = () => {
+bckrBtn.classList.add ('red-background') ;
+};
+bckrBtn.addEventListener ('click', changeColor); */
+
+
+
+// part 3
 const toggleColor = () => {
     bckrBtn.classList.toggle('red-background');
 };
