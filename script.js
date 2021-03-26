da73935e0be61a4fc0a56ed1b14529238a6eb7ff
llpll <45404963+llpll@users.noreply.github.com>
2021-03-26 09:04:59 +0100

    done

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-console.log("Hello Winc Academy")
 
-let name = 'Elena';
-console.log('Elena');
-
-let number = 4 + 4;
-console.log(number);
-
-let string = '4 + 4';
-console.log(string);
-
-let substraction = 90 - 10;
-console.log(substraction);
-
-let multiplication = 3 * 20;
-console.log(multiplication);
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 
-let age = 30;
-console.log(age)
+let log = document.getElementById("message");
 
-let leeftijd = 30;
-console.log(typeof leeftijd);
+let playerName = prompt('Welkom! Wat is je naam?'); 
+log.innerHTML = "Hey " + playerName + " !</br>"; 
 
-let leeftijdString = '30';
-console.log(typeof leeftijdString);
+let roundCount = 1;
+let totalTries = 5;
 
+while (roundCount <= totalTries) {
+	let guessedNumber = prompt('Voer een nummer in van 0 tot 25 om te raden');
+	let randomNumber = getRandomInt(0, 25);
+  if (guessedNumber == randomNumber) {
+    log.innerHTML += 'Your number is ' + guessedNumber + '! ' + 'Gefeliciteerd, je hebt gewonnen!</br>';
+    break;
+  } else {
+    log.innerHTML += 'Your number is ' + guessedNumber + '. ' + ' Nog niet correct, probeer opnieuw.</br>';
 
+  }
 
+  log.innerHTML += (totalTries - roundCount) + ' tries left</br>';
+  roundCount++;
+}
 
-
-
+log.innerHTML += 'Thank you for playing, ' + playerName + '!';
