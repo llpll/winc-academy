8cdd23ea66045afaf30af27f88b33dddc440546d
llpll <45404963+llpll@users.noreply.github.com>
2021-04-09 22:32:11 +0200

    done

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
-console.log("Hello Winc Academy")
+const addTheWordCool = function (array) {
+  array.push('cool');
 
-let name = 'Elena';
-console.log('Elena');
-
-let number = 4 + 4;
-console.log(number);
-
-let string = '4 + 4';
-console.log(string);
-
-let substraction = 90 - 10;
-console.log(substraction);
-
-let multiplication = 3 * 20;
-console.log(multiplication);
-
-
-let age = 30;
-console.log(age)
-
-let leeftijd = 30;
-console.log(typeof leeftijd);
-
-let leeftijdString = '30';
-console.log(typeof leeftijdString);
+  return array;
+}
+console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+// resultaat: ["nice", "awesome", "tof", "cool"]
 
 
 
+const amountOfElementsInArray = function (array) {
+  return array.length;
+}
+console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen']));
+// 3
+
+
+const selectBelgiumFromBenelux = function (array) {
+  return array[0];
+}
+console.log(selectBelgiumFromBenelux(["Belgie", "Nederland", "Luxemburg"]));
+// resultaat: "Belgie"
+
+
+const lastElementInArray = function (array) {
+  return array[array.length - 1];
+}
+console.log(lastElementInArray(["Haas", "Cavia", "Kip", "Schildpad"]));
+// resultaat: "Schildpad"
 
 
 
+const presidents = ["Trump", "Obama", "Bush", "Clinton"]
+
+const impeachTrumpSlice = function (array) {
+  return res = array.slice(1);
+}
+
+const impeachTrumpSplice = function (array) {
+  array.splice(0, 1);
+
+  return array;
+}
+
+console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
+
+
+
+const stringsTogether = function (array) {
+  return array.join(" ");
+}
+console.log(stringsTogether(['Winc', 'Academy', 'is', 'leuk', ';-}']))
+//resultaat: "Winc Academy is leuk ;-}"
+
+
+const combineArrays = function (array1, array2) {
+  return array1.concat(array2);
+}
+console.log(combineArrays([1, 2, 3], [4, 5, 6]));
+// resultaat: [1,2,3,4,5,6]
