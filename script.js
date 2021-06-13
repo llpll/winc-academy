75d0f77acd691bf2196dab60fd4138385aa7d453
llpll <45404963+llpll@users.noreply.github.com>
2021-06-13 15:46:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-console.log("Hello Winc Academy")
-
-let name = 'Elena';
-console.log('Elena');
-
-let number = 4 + 4;
-console.log(number);
-
-let string = '4 + 4';
-console.log(string);
-
-let substraction = 90 - 10;
-console.log(substraction);
-
-let multiplication = 3 * 20;
-console.log(multiplication);
 
 
-let age = 30;
-console.log(age)
+function huiswerkMaken(vak, callback) {
+	console.log('Ok, ok, ik ga nu mijn ' + vak + ' huiswerk maken');
+  setTimeout(function(){ callback(); }, 6000);
+}
 
-let leeftijd = 30;
-console.log(typeof leeftijd);
-
-let leeftijdString = '30';
-console.log(typeof leeftijdString);
-
-
+function klaarMetHuiswerk() {
+	console.log('Ik ben klaar!');
+}
 
 
-
-
+huiswerkMaken('science', klaarMetHuiswerk);
